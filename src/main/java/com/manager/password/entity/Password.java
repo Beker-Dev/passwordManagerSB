6cffdd97964b294c2fbbc5ba788ea811401404d7
Beker-Dev <joaopedro.beker@hotmail.com>
2022-10-06 12:13:58 -0300

    everthing works

--- src/main/java/com/manager/password/entity/Password.java
+++ src/main/java/com/manager/password/entity/Password.java
@@ -32,7 +32,7 @@ public class Password extends AbstractEntity {
 
     @Getter
     @Setter
-    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @ManyToOne(fetch=FetchType.EAGER, optional=false)
     @JoinColumn(name="user_id", nullable=false)
     @OnDelete(action= OnDeleteAction.CASCADE)
     private User user;
